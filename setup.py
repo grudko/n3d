58635cbcd749173c81972c39f148fd89152bcfb6
Anton Grudko <grudko@netrika.ru>
2012-11-29 16:01:12 +0400

    Change Readme and setup files

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 
 setup(
     name='n3d',
-    version='0.1',
-    description='N3 Deployment Tool',
+    version='0.2',
+    description='Utility for step-by-step application deployment',
     author='Anton Grudko',
     author_email='grudko@gmail.com',
-    url='http://develop.netrika.ru/n3d',
+    url="http://pypi.python.org/pypi/n3d",
     platforms=('Any',),
     py_modules=['n3d'],
-    install_requires=['pexpect', 'jinja2', 'termcolor'],
+    install_requires=['pexpect', 'termcolor'],
     entry_points={
         'console_scripts': [
             'n3d = n3d:main',
@@ -23,6 +23,6 @@ setup(
                    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
                    'Operating System :: POSIX',
                    'Programming Language :: Python',
-                   'Topic :: Software Development :: Build Tools'
+                   'Topic :: Software Development :: Build Tools',
                    ]
 )
