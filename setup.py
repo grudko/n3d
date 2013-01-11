2414c584092110d979944de9fb5c557c2b57ab94
Anton Grudko <grudko@netrika.ru>
2013-01-11 17:30:52 +0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='n3d',
-    version='0.2.3',
+    version='0.2.4',
     description='Utility for step-by-step application deployment',
     author='Anton Grudko',
     author_email='grudko@gmail.com',
