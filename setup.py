0e0568767d91672423841c774e101e2f56cfd3bc
Anton Grudko <grudko@netrika.ru>
2014-04-17 14:42:03 +0400

    bump version to 0.3.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='n3d',
-    version='0.3.2',
+    version='0.3.3',
     description='Utility for step-by-step application deployment',
     author='Anton Grudko',
     author_email='grudko@gmail.com',
